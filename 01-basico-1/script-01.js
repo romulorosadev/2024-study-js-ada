716358441c1a50433a5f1e61009bb0dba9ddc8ff
Romulo Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-01-03 17:22:36 -0300

    modulo-01 script-01 hello world

---
+++ 01-basico-1/script-01.js
@@ -0,0 +1,7 @@
+// Primeiro Código - Hello World
+// console.log é o comando de saída de dados mais básico do JS
+// Através do console.log exibimos uma informação/log no exemplo abaixo um texto/string no console do browser Google Chrome
+// Executamos o script com Node JS através da extensão Code Runner do VS Code
+// Usamos console acessando o método .log, mas existem vários métodos com outras funcionalidades. Ex.: console.table
+
+console.log("Welcome to Rio!");
